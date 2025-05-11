12c3d30df404747a3f1825199a1b434edd32f37c
Arindal <160192514+arindalC@users.noreply.github.com>
2025-05-11 14:23:04 -0600

    Update Consultas_análisis_proyecto.sql

--- Consultas_análisis_proyecto.sql
+++ Consultas_análisis_proyecto.sql
@@ -4,24 +4,24 @@
 -- I) Consultas simples para tener un mejor entendimiento de la base y el contexto:
 -- 1. Tipos de vehículos más comunes en accidentes
 SELECT v.vehicle_type AS "Vehículo", COUNT(av.accident_id) AS "Total Accidentes"
-FROM cleaned_road_data.accident_vehicle av
-JOIN cleaned_road_data.vehicle v ON av.vehicle_id = v.id
+FROM public.accident_vehicle av
+JOIN public.vehicle v ON av.vehicle_id = v.id
 GROUP BY v.vehicle_type
 ORDER BY COUNT(av.accident_id) DESC
 LIMIT 5;
 
 -- 2. Top 5 tipos de cruces más peligrosos
 SELECT j.junction_type AS "Cruce", COUNT(l.accident_id) AS "Total Accidentes"
-FROM cleaned_road_data.location l
-JOIN cleaned_road_data.junction j ON l.junction_id = j.id
+FROM public.location l
+JOIN public.junction j ON l.junction_id = j.id
 GROUP BY j.junction_type
 ORDER BY COUNT(l.accident_id) DESC
 LIMIT 5;
 
 -- 3. Condiciones climáticas más frecuentes
 SELECT w.weather_type AS "Clima", COUNT(aw.accident_id) AS "Total Accidentes"
-FROM cleaned_road_data.accident_weather aw
-JOIN cleaned_road_data.weather_condition w ON aw.weather_id = w.id
+FROM public.accident_weather aw
+JOIN public.weather_condition w ON aw.weather_id = w.id
 GROUP BY w.weather_type
 ORDER BY COUNT(aw.accident_id) DESC;
 
@@ -57,12 +57,12 @@ SELECT
   COUNT(a.id) as "Total Accidentes",
   AVG(a.number_casualties) as "Lesiones Promedio",
   RANK() OVER(PARTITION BY s.severity_type ORDER BY COUNT(a.id) DESC) as "Ranking"
-FROM cleaned_road_data.accident a
-JOIN cleaned_road_data.severity s ON a.severity_id = s.id
-JOIN cleaned_road_data.location l ON a.id = l.accident_id
-JOIN cleaned_road_data.road_characteristics rc ON l.road_characteristics_id = rc.id
-JOIN cleaned_road_data.road_type_table rt ON rc.road_type_id = rt.id
-JOIN cleaned_road_data.speed_limit_table sl ON rc.speed_limit_id = sl.id
+FROM public.accident a
+JOIN public.severity s ON a.severity_id = s.id
+JOIN public.location l ON a.id = l.accident_id
+JOIN public.road_characteristics rc ON l.road_characteristics_id = rc.id
+JOIN public.road_type_table rt ON rc.road_type_id = rt.id
+JOIN public.speed_limit_table sl ON rc.speed_limit_id = sl.id
 GROUP BY s.severity_type, rt.road_type, sl.speed_limit;
 
 
@@ -75,12 +75,12 @@ SELECT
   wc.weather_type as "Clima",
   COUNT(DISTINCT a.id) as total_accidentes,
   COUNT(DISTINCT CASE WHEN s.severity_type = 'Fatal' THEN a.id END) as accidentes_fatales
-FROM cleaned_road_data.accident a
-JOIN cleaned_road_data.accident_light al ON a.id = al.accident_id
-JOIN cleaned_road_data.light_condition lc ON al.light_id = lc.id
-JOIN cleaned_road_data.accident_weather aw ON a.id = aw.accident_id
-JOIN cleaned_road_data.weather_condition wc ON aw.weather_id = wc.id
-JOIN cleaned_road_data.severity s ON a.severity_id = s.id
+FROM public.accident a
+JOIN public.accident_light al ON a.id = al.accident_id
+JOIN publica.light_condition lc ON al.light_id = lc.id
+JOIN public.accident_weather aw ON a.id = aw.accident_id
+JOIN public.weather_condition wc ON aw.weather_id = wc.id
+JOIN public.severity s ON a.severity_id = s.id
 GROUP BY CUBE(lc.light_type, wc.weather_type)
 ORDER BY total_accidentes DESC;
 
@@ -92,10 +92,10 @@ WITH junction_analysis AS (
     s.severity_type as gravedad,
     COUNT(*) as accidentes,
     SUM(a.number_casualties) as victimas
-  FROM cleaned_road_data.location l
-  JOIN cleaned_road_data.junction j ON l.junction_id = j.id
-  JOIN cleaned_road_data.accident a ON l.accident_id = a.id
-  JOIN cleaned_road_data.severity s ON a.severity_id = s.id
+  FROM public.location l
+  JOIN public.junction j ON l.junction_id = j.id
+  JOIN public.accident a ON l.accident_id = a.id
+  JOIN public.severity s ON a.severity_id = s.id
   GROUP BY ROLLUP(j.junction_type, s.severity_type)
   )
 SELECT 
@@ -134,10 +134,10 @@ ORDER BY "Total Accidentes" DESC;
 SELECT 
   v.vehicle_type AS "Tipo de Vehículo",
   COUNT(*) AS "Accidentes Fatales"
-FROM cleaned_road_data.accident_vehicle av
-JOIN cleaned_road_data.accident a ON av.accident_id = a.id
-JOIN cleaned_road_data.severity s ON a.severity_id = s.id
-JOIN cleaned_road_data.vehicle v ON av.vehicle_id = v.id
+FROM public.accident_vehicle av
+JOIN public.accident a ON av.accident_id = a.id
+JOIN public.severity s ON a.severity_id = s.id
+JOIN public.vehicle v ON av.vehicle_id = v.id
 WHERE s.severity_type = 'Fatal'
 GROUP BY v.vehicle_type
 ORDER BY "Accidentes Fatales" DESC;
